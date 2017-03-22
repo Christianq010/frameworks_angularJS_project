8b224c3566a57fb4a1afc0be735195f12fa3a664
Christiaan Quyn <christianq010@gmail.com>
2017-03-22 19:17:07 +0530

    feat: Add items to array and ng-repeat, ng-source and ng-click functionality
    ng-repeat to loop over array items, ng-source for images, ng-click to increment / decrement rating upon a button click

--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -9,8 +9,35 @@
  */
 angular.module('udaciMealsApp')
   .controller('MenuCtrl', function () {
-    this.id = 'strawberry-pudding';
-    this.name = 'Strawberry-Pudding';
-    this.img = 'strawberry-pudding.jpg';
-    this.rating = 4.2;
+  this.items = [
+      {
+          id: 'strawberry-pudding',
+          name: 'Strawberry-Pudding',
+          img: 'yeoman.png',
+          calories: 430,
+          rating: 4.2
+      },
+      {
+          id: 'choc-pudding',
+          name: 'Chocolate-Pudding',
+          img: 'strawberry-pudding.jpg',
+          calories: 200,
+          rating: 3
+      },
+      {
+          id: 'choc-cake',
+          name: 'Chocolate-Cake',
+          img: 'strawberry-pudding.jpg',
+          calories: 500,
+          rating: 1.2
+      }
+    ];
+      
+      this.increment = function(item) {
+          item.rating = ((item.rating * 10) + 1) /10;
+      };
+      this.decrement = function(item) {
+          item.rating = ((item.rating * 10) - 1) /10;
+      };
+
   });
