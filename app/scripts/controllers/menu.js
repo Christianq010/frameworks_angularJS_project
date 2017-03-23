42c9d3bf289a12e86d50e54046d2b7855a6996bb
Christiaan Quyn <christianq010@gmail.com>
2017-03-23 17:13:34 +0530

    feat: array changed to .JSON, Add service to controller

--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -8,36 +8,18 @@
  * Controller of the udaciMealsApp
  */
 angular.module('udaciMealsApp')
-  .controller('MenuCtrl', function () {
-  this.items = [
-      {
-          id: 'strawberry-pudding',
-          name: 'Strawberry-Pudding',
-          img: 'yeoman.png',
-          calories: 430,
-          rating: 4.2
-      },
-      {
-          id: 'choc-pudding',
-          name: 'Chocolate-Pudding',
-          img: 'strawberry-pudding.jpg',
-          calories: 200,
-          rating: 3
-      },
-      {
-          id: 'choc-cake',
-          name: 'Chocolate-Cake',
-          img: 'strawberry-pudding.jpg',
-          calories: 500,
-          rating: 1.2
-      }
-    ];
-      
+  .controller('MenuCtrl', ['foodFinder', function(menu) {
+      var vm = this;
+
+      menu.getMenu().then(function(data) {
+          vm.items = data;
+      });
+
       this.increment = function(item) {
-          item.rating = ((item.rating * 10) + 1) /10;
+          item.rating = ((item.rating * 10) + 1) / 10;
       };
       this.decrement = function(item) {
-          item.rating = ((item.rating * 10) - 1) /10;
+          item.rating = ((item.rating * 10) - 1) / 10;
       };
 
-  });
+  }]);
